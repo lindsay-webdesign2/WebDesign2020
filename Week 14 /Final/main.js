d54062a2d791c4577867f6471615fa5bdb59bde5
Lindsay Roach <roach_lindsay_mabel@student.smc.edu>
2020-12-14 23:02:31 -0800

    Final Brief
    Beginning layout and concept

---
+++ Week 14 /Final/main.js
@@ -0,0 +1,70 @@
+var firstName = "";
+		var lastName = "";
+		var email = "";
+		var dType = "";
+		var receipt = "";
+		var anon = "";
+		var list = "";
+		var amount = "";
+		
+		$('.set-amount').autoGrow(0);
+		
+		/*
+			if(isiPad || jQuery.browser.mobile){
+				$('#team').hide()
+				$('.team-mobile').show();	
+			}else{
+				$('#team').show()
+				$('.team-mobile').hide();
+			}
+		*/
+		
+		//Set & Highlight Donation Amount
+		$(".button").click(function(){
+			$(".button").removeClass("selected");
+			$(this).addClass("selected");
+			
+			$(this).find("input").focus();
+		});
+		
+		//Grow the donation box if they type more than 4 numbers
+		$(".set-amount").keyup(function(){
+			
+			if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+		       this.value = this.value.replace(/[^0-9\.]/g, '');
+		    }
+
+		});
+		
+		
+		$("input").on("change", function(){
+			// $(".donation-box").css("height", "500px");
+			
+			firstName = $("#firstName").val();
+			lastName = $("#lastName").val();
+			email = $("#email").val();
+			
+			if ( $("#one-time").prop( "checked" ) ){
+				dType = "One-Time";
+			}
+			if ( $("#monthly").prop( "checked" ) ){
+				dType = "Monthly";
+			}
+
+        });
+        
+        var count = 5;
+    var redirect = "index.html";
+     
+    function countDown(){
+        var timer = document.getElementById("timer");
+        if(count > 0){
+            count--;
+            timer.innerHTML = "This page will redirect in "+count+" seconds.";
+            setTimeout("countDown()", 1000);
+        }else{
+            window.location.href = redirect;
+        }
+    }
+    countDown();
+    </script>
